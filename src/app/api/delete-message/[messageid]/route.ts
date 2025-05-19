d1d34de7b7125af0580cdef0920ff6ddcc31932a
hemantyadav8006 <hemantyadav8006@gmail.com>
2025-05-19 12:04:04 +0530

    add delete api

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -37,7 +37,6 @@ export async function DELETE(
       { status: 200 }
     );
   } catch (error) {
-    console.error("Error deleting message:", error);
     return Response.json(
       { message: "Error deleting message", success: false },
       { status: 500 }
