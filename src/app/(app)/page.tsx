3b4fd1f5c9385403b2b6982cd942de4fafa5cadf
hemantyadav8006 <hemant.kumar@kollegeapply.com>
2025-05-23 18:27:33 +0530

    Update homepage text and carousel autoplay delay

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -16,15 +16,15 @@ export default function Home() {
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
-            Dive into the World of Anonymous Feedback
+            Explore the World of Anonymous Reviews
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
-            True Feedback - Where your identity remains a secret.
+            True Feedback - where you don't reveal who you are.
           </p>
         </section>
 
         <Carousel
-          plugins={[Autoplay({ delay: 1500 })]}
+          plugins={[Autoplay({ delay: 2000 })]}
           className="w-full max-w-lg md:max-w-xl"
         >
           <CarouselContent>
@@ -51,7 +51,10 @@ export default function Home() {
       </main>
 
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2025 Mystery Message | True Feedback. All rights reserved.
+        © 2025 Mystery Message | True Feedback. All rights reserved to{" "}
+        <a className="text-blue-500" href="https://github.com/hemantyadav8006">
+          Hemant Yadav
+        </a>
       </footer>
     </>
   );
